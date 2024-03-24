2f89719a1e99548e8ca13081f775ee1f1620d2f8
svdgoor <sjoerdvdgoor02@gmail.com>
2024-03-24 11:22:43 +0100

    model in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,7 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+
 const String spOpenApiKey = 'openApiKey';
 const String assistantInstruction =
     "You are assisting users with their medicine usage. Please be professional, clear, and understandable in your responses. Assume the information in the files you have. Ask for further clarification from the user if needed to answer well. Offer guidance on dosage instructions, potential side effects, and medication interactions in simple language. Sometimes thank them for using the service. Encourage them to ask for clarification if needed.";
 const String assistantName = "Docter Robert";
+const String openAIModel = kGpt41106Preview;
